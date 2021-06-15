e2cb39c4648719390fd9a697914d5b0b644261e3
asciphx <42582375+asciphx@users.noreply.github.com>
2021-06-15 18:46:24 +0800

    Bug Fix

--- c/bigNum/mul.c
+++ c/bigNum/mul.c
@@ -7,23 +7,18 @@ char*bigMul(const char*m,const char*n){
   a=m[0]=='-';b=n[0]=='-';f=a&&!b||!a&&b;
   for(x=a;m[x];++x)
     for(y=b;n[y];++y){
-      t=x+y+f-z-b;
+      t=x+y+f-a-b;
       j[t]+=(m[x]-0x30)*(n[y]-0x30);
-      if(j[t]<0){
-        j[t-1]+=(j[t]+0x100)/10;
-        j[t]=(j[t]+0x100)%10;
-      }
+      if(j[t]<0) j[t-1]+=(j[t]+0x100)/10,j[t]=(j[t]+0x100)%10;
     }
-  for(i=z=x+y-1+f;i>f;){
-    if(j[--i]<0){ 
-      t=j[i]+0x100;j[i-1]+=t/10;j[i]=t%10;
-    }
+  for(i=z=x+y+f-a-b-1;i>f;){
+    if(j[--i]<0) t=j[i]+0x100,j[i-1]+=t/10,j[i]=t%10;
     if(j[i]<10){ j[i]+=0x30;continue; }
-    if(i>f)j[i-1]+=j[i]/10;else{y=0,x=j[i]/10;} 
+    if(i>f)j[i-1]+=j[i]/10;else y=0,x=j[i]/10;
     j[i]%=10;j[i]+=0x30;
   }
-  if(f)j[0]=0x2d;
-  if(!y){j[z+f+1]=0;while(z--)j[z+1+f]=j[z+f];j[f]=x+48;}
+  if(f) j[0]=0x2d;
+  if(!y){t=f+1;j[z+t]=0;while(z--)j[z+t]=j[z+f];j[f]=x+0x30;}
   return j;
 }
 int main(){
